456c2aa21e6173623859ed2e2187ff96f3218817
sunti <sunti.martin@gmail.com>
2026-06-22 18:25:35 +0200

    change code of telecomande (for test)

--- Remote/Remote.ino
+++ Remote/Remote.ino
@@ -1,13 +1,15 @@
+#include <SoftwareSerial.h>
+
 #define JOYSTICK1_Y A0
 #define JOYSTICK1_X A1
 #define JOYSTICK2_Y A2
 #define JOYSTICK2_X A3
 #define JOYSTICK1_SW 6 //2 !
-#define JOYSTICK2_SW 3
+//#define JOYSTICK2_SW 3
 
 #define R_ENCODER_SW  4
 #define R_ENCODER_DT  5
-#define R_ENCODER_CLK 2 //6 !
+//#define R_ENCODER_CLK 11 //6 !
 
 #define BUTTON1 7
 #define BUTTON2 8
@@ -15,6 +17,8 @@
 #define BUTTON4 10
 
 #define LED 13
+
+SoftwareSerial HC12(2, 3);
 
 unsigned char myMessage[13]; //data
 
@@ -25,6 +29,8 @@ unsigned long tempsA;
 unsigned long lastMessage = 0;
 
 void setup() {
+  HC12.begin(9600);
+  delay(500);
   Serial.begin(9600);
 
   pinMode(JOYSTICK1_X, INPUT);
@@ -36,23 +42,26 @@ void setup() {
   pinMode(BUTTON3, INPUT_PULLUP);
   pinMode(BUTTON4, INPUT_PULLUP);
   pinMode(JOYSTICK1_SW, INPUT_PULLUP);
-  pinMode(JOYSTICK2_SW, INPUT_PULLUP);
+  //pinMode(JOYSTICK2_SW, INPUT_PULLUP);
   pinMode(R_ENCODER_SW, INPUT_PULLUP);  // SW ENCODER
-  pinMode(R_ENCODER_CLK, INPUT);
+  //pinMode(R_ENCODER_CLK, INPUT);
   pinMode(R_ENCODER_DT, INPUT);
 
   pinMode(LED, OUTPUT);
 
   // état de A au setup
-  dernierEtatA = digitalRead(R_ENCODER_CLK);
+  //dernierEtatA = digitalRead(R_ENCODER_CLK);
   deltaEncoder = 0;
   // memorisation du temps pour eviter des erreurs de changements d'etat
   tempsA = millis();
   lastMessage = millis();
-  attachInterrupt(digitalPinToInterrupt(R_ENCODER_CLK), changementA, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(R_ENCODER_CLK), changementA, CHANGE);
 }
 
 void loop() {
+
+  Serial.print("HC12 available: ");
+  Serial.println(HC12.available());
 
   if (millis() > lastMessage + 200)
     digitalWrite(LED, (millis()%400) < 150);
@@ -61,9 +70,9 @@ void loop() {
 
   char val[1]; //Used to store the incomming message from robot
   val[0] = 0;
-  if (Serial.available() > 0)
+  if (HC12.available() > 0)
   {
-    Serial.readBytes(val, 1);
+    HC12.readBytes(val, 1);
     if (val[0] == 65)
     {
       myMessage[0] = 65;
@@ -76,32 +85,32 @@ void loop() {
       myMessage[7] = digitalRead(BUTTON3);
       myMessage[8] = digitalRead(BUTTON4);
       myMessage[9] = digitalRead(JOYSTICK1_SW);
-      myMessage[10] = digitalRead(JOYSTICK2_SW);
+      myMessage[10] = 0;//digitalRead(JOYSTICK2_SW);
       myMessage[11] = digitalRead(R_ENCODER_SW);
       myMessage[12] = 0x55;
       deltaEncoder = 0;
 
-      Serial.write(myMessage,13); //Write the serial data
+      HC12.write(myMessage,13); //Write the serial data
       lastMessage = millis();  //Remember when was the last time you recieved a mess
     }
   }
 }
 
 void changementA(){
-  etatA = digitalRead(R_ENCODER_CLK);
+  //etatA = digitalRead(R_ENCODER_CLK);
    
   // controle du temps pour eviter des erreurs 
   if( abs(millis() - tempsA) > 50 ){
     // Si B different de l'ancien état de A alors
-    if(digitalRead(R_ENCODER_DT) != dernierEtatA){
+    /*if(digitalRead(R_ENCODER_DT) != dernierEtatA){
       deltaEncoder--;
     }
     else{
       deltaEncoder++;
-    }
+    }*/
     // memorisation du temps pour A
     tempsA = millis();
   } 
   // memorisation de l'état de A
-  dernierEtatA = etatA ;
+  //dernierEtatA = etatA ;
 }
